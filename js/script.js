746fee9afeea12f7d011a01e0063410c1f07e1a8
CheshirCode <50378606+CheshirCode@users.noreply.github.com>
2021-02-03 14:40:17 +0300

    trouble menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 // "use strict"
 $(document).ready(function () {
-    $('.header__burger').click(function (event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
+  $(".header__burger").click(function (event) {
+    $(".header__burger,.header__menu").toggleClass("active");
+    // $("body").toggleClass("lock");
+  });
 });
-
-
